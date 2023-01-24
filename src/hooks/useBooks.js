44f0e99cc819b8ad323288b76d71ae33e6324f4f
Akilah Powell <atpjun@gmail.com>
2023-01-23 17:29:48 -0800

    reverting some changes

--- src/hooks/useBooks.js
+++ src/hooks/useBooks.js
@@ -1,35 +1,23 @@
 import { useEffect, useState } from 'react';
 import { getBooks } from '../services/books';
-import { Link } from 'react-router-dom';
-import Book from '../components/book/Book';
-
 
 export function useBooks() {
   const [error, setError] = useState('');
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(true);
 
-
   useEffect(() => {
-    getBooks()
-      .then(({ data }) => setBooks(data))
-      .catch((error) => setError(error))
-      .finally(() => setLoading(false));
+    const fetchData = async () => {
+      try {
+        const data = await getBooks();
+        setBooks(data);
+        setLoading(false);
+      } catch (e) {
+        setError(e.message);
+      }
+    };
+    fetchData();
   }, []);
-  if (loading) return <h1>Loading...</h1>;
 
-  return (
-    <section className="books">
-      <Link to="/">&laquo; Back to Homepage</Link>
-      <ul className="use-books" aria-label="book list">
-        {books.map((book) => (
-          <li key ={book.book_id}>
-            <Link to={`/books/${book.book_id}`}></Link>
-            <Book book={book} />
-          </li>
-        ))}
-      </ul>
-    </section>
-  );
+  return { error, books, loading };
 }
-export default useBooks;
