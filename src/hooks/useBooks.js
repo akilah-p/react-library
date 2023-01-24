3a896ca575737c44fc09e221c946b52e5e453667
Akilah Powell <atpjun@gmail.com>
2023-01-23 17:10:57 -0800

    added route linking to use books file

--- src/hooks/useBooks.js
+++ src/hooks/useBooks.js
@@ -1,23 +1,35 @@
 import { useEffect, useState } from 'react';
 import { getBooks } from '../services/books';
+import { Link } from 'react-router-dom';
+import Book from '../components/book/Book';
+
 
 export function useBooks() {
   const [error, setError] = useState('');
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await getBooks();
-        setBooks(data);
-        setLoading(false);
-      } catch (e) {
-        setError(e.message);
-      }
-    };
-    fetchData();
-  }, []);
 
-  return { error, books, loading };
+  useEffect(() => {
+    getBooks()
+      .then(({ data }) => setBooks(data))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
+  if (loading) return <h1>Loading...</h1>;
+
+  return (
+    <section className="books">
+      <Link to="/">&laquo; Back to Homepage</Link>
+      <ul className="use-books" aria-label="book list">
+        {books.map((book) => (
+          <li key ={book.book_id}>
+            <Link to={`/books/${book.book_id}`}></Link>
+            <Book book={book} />
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
+export default useBooks;
