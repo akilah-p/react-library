67fdcd145e75327fb4d055f62689527ee7521e6b
Akilah Powell <atpjun@gmail.com>
2023-01-24 02:20:17 -0800

    list rendering again after importering switch and routes into app. js but links not working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import BookList from './components/book/BookList';
 import BookDetail from './components/book/BookDetail';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 
@@ -9,7 +9,11 @@ function App() {
   return (
     <main className="container">
       <h1>Library Catalog</h1>
-      <BookList />
+      <Switch>
+        <Route exact path ="/" component={BookList} />
+        <Route exact path="/books" component={BookList} />
+        <Route exact path ="/books/:id" component={BookDetail} />
+      </Switch>
     </main>
   );
 }
